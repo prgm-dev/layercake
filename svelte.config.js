e87b562d63dac6e7d115b1583d079e2dffd7a362
Alexandre Jouandin <alexandre@jouand.in>
2022-12-06 11:46:29 +0000

    Upgrade most dependencies
    This will most probably break the docs that relied on SvelteKit next-200

--- svelte.config.js
+++ svelte.config.js
@@ -1,51 +1,15 @@
-import { normalizePath } from 'vite';
-import path from 'path';
-import dsv from '@rollup/plugin-dsv';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	root: normalizePath(path.resolve('./')),
-
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'docs',
-			fallback: null
-		}),
+	kit: { adapter: adapter() },
 
-		target: 'body',
-		package: {
-			exports: (filepath) => {
-				return filepath.endsWith('index.js')
-			}
-		},
-
-		vite: {
-			plugins: [
-				dsv()
-			],
-			resolve: {
-				alias: {
-					'layercake': [path.resolve('src/lib')]
-				}
-			}
-		}
-  	// 	optimizeDeps: {
-  	// 	  include: [
-		// 			'd3-scale',
-		// 			'd3-array',
-		// 			'd3-format',
-		// 			'd3-interpolate',
-		// 			'd3-time',
-		// 			'd3-time-format'
-		// 		]
-		//   }
-		// }
-	}
+	alias: { layercake: 'src/lib' }
 };
 
 export default config;
