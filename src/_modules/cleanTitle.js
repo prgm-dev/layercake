dcf8fb72e4aeeac3d811e3361f2b6835e7802c3c
Alexandre Jouandin <alexandre@jouand.in>
2022-12-06 11:29:25 +0000

    Remove Docs-related files
    This commit should be reverted before merging upstream.
    It has been done so that the library dependencies can be upgraded more easily.
    They will have to undergo a migration prior to being re-added.

--- src/_modules/cleanTitle.js
+++
@@ -1,7 +0,0 @@
-export default function cleanTitle(title) {
-	const parts = title.split('/');
-	const nameParts = parts[parts.length - 1].split('.')
-	nameParts.pop();
-	const cleaned = nameParts.join('.').toLowerCase();
-	return cleaned;
-}
